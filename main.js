2cf0a6e57a86bd648db6f4910bd0155d6c5e2a03
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2024-04-17 17:37:12 -0300

    feat(frontend):tornando o cracha responsivo

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 //cameCase, PascalCase, snake_case//
 
-const LinksSocialMedia ={
-  github: "LipeBiondes",
-  youtube: "channel/UC3wB4c8dfhtVOPY1ujut_fA",
-  facebook:  "alefe.felipe.336",
-  instagram: "lipe_biondes",
-  twitter: "Lipe_pereira"
+const LinksSocialMedia = {
+  github: 'LipeBiondes',
+  youtube: 'channel/UC3wB4c8dfhtVOPY1ujut_fA',
+  facebook: 'alefe.felipe.336',
+  instagram: 'lipe_biondes',
+  twitter: 'Lipe_pereira'
 }
 function changeSocialMediaLinks() {
-  for(let li of socialLinks.children) {
-    const social=li.getAttribute('class')
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
 
     li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
   }
 }
 changeSocialMediaLinks()
 
-function getGitHubProfileInfos(){
+function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${LinksSocialMedia.github}`
 
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    userName.textContent = data.name
-    userBio.textContent = data.bio
-    userLink.href = data.html_url
-    userPhoto.src = data.avatar_url
-    userLoginGit.textContent = data.login
-  })
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLoginGit.textContent = data.login
+    })
 }
 
 getGitHubProfileInfos()
